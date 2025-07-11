3c243b62b8ea4e7b3f6a1b990fce3bf8dd26841d
juanjbsalas <jbsalas05@gmail.com>
2025-07-11 14:42:57 -0400

    testing deployment

--- home/nav.js
+++ home/nav.js
@@ -1,4 +1,5 @@
-fetch('home/nav.html')
+// fetch('/travel-dest/home/nav.html')
+fetch(window.location.pathname.includes('/travel-dest') ? '/travel-dest/home/nav.html' : 'home/nav.html')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_navbar");
@@ -22,3 +23,4 @@ fetch('home/nav.html')
     });
 
 });
+console.log('It is RUNNING!!')
