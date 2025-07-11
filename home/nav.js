db2c4db4578019e4f016851266666a8b25274603
juanjbsalas <jbsalas05@gmail.com>
2025-07-11 13:53:59 -0400

    fixed hamburguer button clickability

--- home/nav.js
+++ home/nav.js
@@ -5,4 +5,20 @@ fetch('home/nav.html')
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem, oldelem);
-})
+
+    // Event listener for hambuger button click
+    const toggleButton = document.getElementById('menu-toggle');
+    const menuIcon = document.getElementById('menu-icon');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    toggleButton.addEventListener('click', function(){
+        mobileMenu.classList.toggle('hidden');
+
+        if(menuIcon.classList.contains('fa-bars')) {
+            menuIcon.classList.replace('fa-bars', 'fa-xmark');
+        } else {
+            menuIcon.classList.replace('fa-xmark', 'fa-bars');
+        }
+    });
+
+});
